f362cbb58758891a7ae46a8d83ebb3e77ef1ca63
Jeffrey Horn <jhorn@theloud.com>
2018-10-17 22:16:18 -0400

    need to work on OAuth

--- spotify-connect-trial/src/App.js
+++ spotify-connect-trial/src/App.js
@@ -1,15 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const SpotifyWebAPI = require('spotify-web-api-node')
+
+const spotifyAPI = new SpotifyWebAPI() 
 
 class App extends Component {
+
+  cheese = () => {
+    spotifyAPI.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+      .then(function(data) {
+        console.log('hello, test')
+        console.log('Artist albums', data.body);
+      }, function(err) {
+        console.error(err);
+      });
+  }
+
   render() {
+    this.cheese()
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            HELLO
           </p>
           <a
             className="App-link"
@@ -17,12 +33,12 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            CHEESE
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
